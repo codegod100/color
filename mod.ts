6ed625c2acd9feb4b50392e2b4d6cb6e33910cb2
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-12-17 22:14:12 -0800

    version bump

--- mod.ts
+++ mod.ts
@@ -2,11 +2,5 @@
  * @module
  * This module provides a function to colorize strings in the console.
  *
- * @example
- * ```ts
- * import { color } from "jsr:@nandi/color";
- * color("this outputs a random color"); // random color
- * color("this outputs a red color", "red"); // red color
- * ```
  */
-export * from "./main.ts";
+export { color } from "./main.ts";
